3a73f8d66998f1e3f4d8dc8590c75553e1b4244e
123jayc <95005899+123jayc@users.noreply.github.com>
2025-08-27 15:08:13 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "qwerty_123"
+password = "qwerty123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
