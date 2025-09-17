6d5590a765e3c85d245bb1ae3e21523facd1f6c0
123jayc <95005899+123jayc@users.noreply.github.com>
2025-09-17 10:37:23 +0800

    Add files via upload

---
+++ code_challenge8.py
@@ -0,0 +1,15 @@
+#Create a program that prints the of a number entered by the user, using a simple for loop.
+#Ask the user to input a number.Use a for loop to print the multiplication table from for that number.
+# Format each line clearly (e.g., 5 x 1 = 5).
+
+#This reinforces:
+#Looping over a fixed range (range(1, 11))
+#Using the loop variable in calculations
+#String formatting
+
+
+print('MULTIPLICATION TABLE MAKER')
+mul = int(input('Enter a number:'))
+
+for i in range(1, 11):
+    print(mul, 'x', i, '=', mul * i)
